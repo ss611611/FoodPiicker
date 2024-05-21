93b5e43cb9789a89fdb0d15246460290a593cf75
ss611611 <ss611611@gmail.com>
2024-05-21 14:52:44 +0800

    Initial Commit

---
+++ FoodPiicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodPiicker
+//
+//  Created by Jackie Lu on 2024/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodPiicker/FoodPiickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPiickerApp.swift
+//  FoodPiicker
+//
+//  Created by Jackie Lu on 2024/5/21.
+//
+
+import SwiftUI
+
+@main
+struct FoodPiickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
